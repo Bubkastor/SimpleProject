40b6eaac1cc2d32410ec119f7057b4c2d1b7b4b0
Альберт Губкин <albert.gubkin@travelline.ru>
2018-09-12 13:52:13 +0300

    Добавьте файлы проекта.

---
+++ SimpleProject/Model/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleProject.Model
+{
+    public enum Sex
+    {
+        none,
+        female,
+        man
+
+    }
+    public class UserModel
+    {
+        [DisplayAttribute(Name ="Name")]
+        [Required]
+        public String Name { get; set; }
+
+        [DisplayAttribute(Name = "Age")]
+        public int Age { get; set; }
+
+        [DisplayAttribute(Name = "Sex")]
+        public Sex Sex { get; set; }
+
+    }
+}
---
+++ SimpleProject/Pages/Index.cshtml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleProject.Model;
+
+namespace SimpleProject.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [ModelBinder]
+        public UserModel User { get; set; }
+
+        [TempData]
+        public String Message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        [HttpPost]
+        [ActionName("simple")]
+        public void OnPostSimple()
+        {
+            if (ModelState.IsValid)
+            {
+                Message = "Valid Simple";
+            }
+            else
+            {
+                Message = "Not valid Simple";
+            }
+        }
+
+        [HttpPost]
+        [ActionName("template")]
+        public void OnPostTemplate()
+        {
+            if (ModelState.IsValid)
+            {
+                Message = "Valid Template";
+            }
+            else
+            {
+                Message = "Not valid Template";
+            }
+        }
+    }
+}
